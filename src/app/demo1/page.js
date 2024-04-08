c2468b3820bd2356319155dfa39a50b922ea226e
Subhash Pandey <su8hash@gmail.com>
2024-04-08 21:49:01 +0530

    Changes

--- src/app/demo1/page.js
+++ src/app/demo1/page.js
@@ -1,5 +1,10 @@
+import Link from "next/link";
 
 export default function Page() {
-    return <p>D<h1>This is page 1 of application</h1>
-    <p>Keep Browsing</p></p>;
+    return <p><h1>This is page 1 of application</h1>
+    <p>Keep Browsing</p>
+    
+    <Link href={"/demo2"}>This will take to second page</Link>
+    </p>;
+    
   }
--- src/app/page.js
+++ src/app/page.js
@@ -93,7 +93,7 @@ export default function Home() {
 
 
         <Link
-          href="/demo1/"
+          href="/demo1"
           
       
      
