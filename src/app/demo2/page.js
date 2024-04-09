abebc7c45b63c9aeecee9a8a7fc2fc88fbeb3605
Subhash Pandey <su8hash@gmail.com>
2024-04-09 19:26:24 +0530

    changes to raise an alert

--- src/app/demo2/page.js
+++ src/app/demo2/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 export default function RandomName(){
     return <> <span>This is Page 2</span>
+     <button onClick={()=>alert("hey")}>+Plus+</button>
        <Link href={"/demo"}>visit a client page</Link> </>
 }
